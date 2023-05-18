78a79b09997d6bdcf23bb277d758b80bceed8d2a
zlatan1487 <vitalik.karpukhin@list.ru>
2023-05-18 16:58:55 +0200

    sixth task "youtube-analytics_project" changed

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,5 +2,6 @@ from src.video import Video
 
 if __name__ == '__main__':
     broken_video = Video('broken_video_id')
+    print(broken_video.channelId)
     assert broken_video.title is None
     assert broken_video.like_count is None
--- src/video.py
+++ src/video.py
@@ -1,30 +1,40 @@
 from src.mixin import Path
+from googleapiclient.errors import HttpError
 
 
 class Video(Path):
-
     def __init__(self, video_id):
         self.video_id = video_id
-        self.service = self.get_videos
+        self.service = self.get_videos()
 
-        self.channelId = self.service()['items'][0]['snippet']['channelId']
-        self.title = self.service()['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/channel/{self.video_id}'
-        self.video_count = int(self.service()['items'][0]['statistics']['viewCount'])
-        self.like_count = int(self.service()['items'][0]['statistics']['likeCount'])
+        try:
+            video_info = self.service['items'][0]
+            self.channelId = video_info['snippet']['channelId']
+            self.title = video_info['snippet']['title']
+            self.url = f'https://www.youtube.com/channel/{self.video_id}'
+            self.video_count = int(video_info['statistics']['viewCount'])
+            self.like_count = int(video_info['statistics']['likeCount'])
+        except (HttpError, IndexError):
+            self.channelId = None
+            self.title = None
+            self.url = None
+            self.video_count = None
+            self.like_count = None
 
-    def get_videos(self) -> None:
-        """Выводит в консоль информацию о канале"""
+    def get_videos(self):
+        """Returns channel information."""
         channel_info = self.response.videos().list(id=self.video_id, part='snippet,statistics').execute()
         return channel_info
 
     def __repr__(self):
-        return f'[this class name: {self.__class__.__name__}]''\n'\
-               f'[channelId: {self.channelId}]''\n'\
-               f'[title: {self.title}]''\n'\
-               f'[url: {self.url}]''\n'\
-               f'[video_count: {self.video_count}]''\n'\
-               f'[like_count: {self.like_count}]'
+        return (
+            f'[this class name: {self.__class__.__name__}]\n'
+            f'[channelId: {self.channelId}]\n'
+            f'[title: {self.title}]\n'
+            f'[url: {self.url}]\n'
+            f'[video_count: {self.video_count}]\n'
+            f'[like_count: {self.like_count}]'
+        )
 
     def __str__(self):
         return f'{self.title}'
